ff330be26b95ab15c4d74e75879b958f7ecb194d
Paul Bellamy <paul.a.bellamy@gmail.com>
2011-04-12 18:52:23 +0100

    running gofmt on stuff

--- comments.go
+++ comments.go
@@ -1,93 +1,101 @@
 package main
 
 import (
-  "time";
-  "fmt";
-  "json";
-  "strconv";
-  "bytes";
-  "os";
-  "log";
+	"time"
+	"fmt"
+	"json"
+	"strconv"
+	"bytes"
+	"os"
+	"log"
 )
 
 type Comment struct {
-  Id        int64;
-  Author    string;
-  Body      string;
-  CreatedAt int64;
-  PageUrl   string;
+	Id        int64
+	Author    string
+	Body      string
+	CreatedAt int64
+	PageUrl   string
 }
 
 func New(j []byte) (c Comment, err os.Error) {
-  err = json.Unmarshal(j, &c);
+	err = json.Unmarshal(j, &c)
 
-  return c, err;
+	return c, err
 }
 
 func Create(j []byte) (c Comment, err os.Error) {
-  c, err = New(j);
-  if err != nil {
-    log.Println(fmt.Sprintf("Error Parsing Comment: %s", err));
-    return c, err;
-  }
+	c, err = New(j)
+	if err != nil {
+		log.Println(fmt.Sprintf("Error Parsing Comment: %s", err))
+		return c, err
+	}
 
-  if err = c.Save(); err != nil {
-    log.Println(fmt.Sprintf("Error Saving Comment: %s", err));
-    return c, err;
-  }
+	if err = c.Save(); err != nil {
+		log.Println(fmt.Sprintf("Error Saving Comment: %s", err))
+		return c, err
+	}
 
-  return c, nil;
+	return c, nil
 }
 
 func Find(id int64) (c Comment, err os.Error) {
-  js, _ := client.Get( fmt.Sprintf("comment:id:%d", id) );
-  return New(js);
+	js, _ := client.Get(fmt.Sprintf("comment:id:%d", id))
+	return New(js)
 }
 
 func PaginateFor(url string, start int, count int) (c []Comment) {
-  commentIds, _ := client.Lrange(fmt.Sprintf("comment:page_url:%s", url), start, count);
-  for _, idString := range commentIds {
-    id, _ := strconv.Atoi64(string(idString));
-    comment, err := Find(id);
-    if (err == nil) {
-      c = append(c, comment);
-    }
-  }
-  return c;
+	commentIds, _ := client.Lrange(fmt.Sprintf("comment:page_url:%s", url), start, count)
+	for _, idString := range commentIds {
+		id, _ := strconv.Atoi64(string(idString))
+		comment, err := Find(id)
+		if err == nil {
+			c = append(c, comment)
+		}
+	}
+	return c
 }
 
 func (c *Comment) ToJson() (j string) {
-  if j, err := json.Marshal(c); err != nil {
-    log.Println("Error Encoding Comment to Json: ", err);
-  } else {
-    return string(j);
-  }
-  return "";
+	if j, err := json.Marshal(c); err != nil {
+		log.Println("Error Encoding Comment to Json: ", err)
+	} else {
+		return string(j)
+	}
+	return ""
 }
 
 func (c *Comment) Save() (err os.Error) {
 
-  newRecord := false;
-  if (c.Id == 0) { newRecord = true; }
+	newRecord := false
+	if c.Id == 0 {
+		newRecord = true
+	}
 
-  if (newRecord) {
-    // New record we should get an Id for it
-    id, err := client.Incr("global:nextCommentId");
-    if (err != nil) { return err; }
-    c.Id = id;
+	if newRecord {
+		// New record we should get an Id for it
+		id, err := client.Incr("global:nextCommentId")
+		if err != nil {
+			return err
+		}
+		c.Id = id
 
-    c.CreatedAt = time.Seconds();
-  }
+		c.CreatedAt = time.Seconds()
+	}
 
-  // Store it by the primary key
-  client.Set(fmt.Sprintf("comment:id:%d", c.Id), []uint8(c.ToJson()));
-  if (err != nil) { return err; }
+	// Store it by the primary key
+	client.Set(fmt.Sprintf("comment:id:%d", c.Id), []uint8(c.ToJson()))
+	if err != nil {
+		return err
+	}
 
-  if (newRecord) {
-    // New record we should insert it into the page listing
-    err :=client.Lpush(fmt.Sprintf("comment:page_url:%s", c.PageUrl), bytes.NewBufferString(strconv.Itoa64(c.Id)).Bytes());
-    if (err != nil) { return err; }
-  }
+	if newRecord {
+		// New record we should insert it into the page listing
+		err := client.Lpush(fmt.Sprintf("comment:page_url:%s", c.PageUrl), bytes.NewBufferString(strconv.Itoa64(c.Id)).Bytes())
+		if err != nil {
+			return err
+		}
+	}
 
-  return nil;
+	return nil
 }
--- server.go
+++ server.go
@@ -1,49 +1,49 @@
 package main
 
 import (
-  "json";
-  "github.com/madari/go-socket.io";
-  "github.com/hoisie/redis.go";
-  "http";
-  "log";
+	"json"
+	"github.com/madari/go-socket.io"
+	"github.com/hoisie/redis.go"
+	"http"
+	"log"
 )
 
-var client redis.Client;
+var client redis.Client
 
-var sio *socketio.SocketIO;
+var sio *socketio.SocketIO
 
 func socketIOConnectHandler(c *socketio.Conn) {
-  j, _ := json.Marshal(PaginateFor("/", 0, 10));
-  c.Send("{\"event\":\"initial\", \"data\":" + string(j) + "}");
+	j, _ := json.Marshal(PaginateFor("/", 0, 10))
+	c.Send("{\"event\":\"initial\", \"data\":" + string(j) + "}")
 }
 
 func socketIODisconnectHandler(c *socketio.Conn) {
 }
 
 func socketIOMessageHandler(c *socketio.Conn, msg socketio.Message) {
-  if comment, err := Create([]uint8(msg.Data())); err == nil {
-    log.Println("Stored Comment: ", comment.ToJson());
-    sio.Broadcast("{\"event\":\"comment\", \"data\":" + comment.ToJson() + "}");
-  } else {
-    log.Println("Error Storing Comment: ", err);
-  }
+	if comment, err := Create([]uint8(msg.Data())); err == nil {
+		log.Println("Stored Comment: ", comment.ToJson())
+		sio.Broadcast("{\"event\":\"comment\", \"data\":" + comment.ToJson() + "}")
+	} else {
+		log.Println("Error Storing Comment: ", err)
+	}
 }
 
 func main() {
-  client.Addr = "localhost:6379";
+	client.Addr = "localhost:6379"
 
-  // create the socket.io server and mux it to /socket.io/
-  config := socketio.DefaultConfig
-  config.Origins = []string{"*"}
-  sio = socketio.NewSocketIO(&config)
-  
-  sio.OnConnect(socketIOConnectHandler);
-  sio.OnDisconnect(socketIODisconnectHandler);
-  sio.OnMessage(socketIOMessageHandler);
+	// create the socket.io server and mux it to /socket.io/
+	config := socketio.DefaultConfig
+	config.Origins = []string{"*"}
+	sio = socketio.NewSocketIO(&config)
 
-  mux := sio.ServeMux();
-  mux.Handle("/", http.FileServer("static/", "/"))
-  if err := http.ListenAndServe(":80", mux); err != nil {
-    log.Fatal("ListenAndServe: ", err);
-  }
+	sio.OnConnect(socketIOConnectHandler)
+	sio.OnDisconnect(socketIODisconnectHandler)
+	sio.OnMessage(socketIOMessageHandler)
+
+	mux := sio.ServeMux()
+	mux.Handle("/", http.FileServer("static/", "/"))
+	if err := http.ListenAndServe(":80", mux); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
