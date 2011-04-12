c2328b6c2c2022e8982b8bd4026f664c1eb04804
Paul Bellamy <paul.a.bellamy@gmail.com>
2011-04-12 22:45:33 +0100

    Making it accept pageUrl differences and handle them

--- static/js/application.js
+++ static/js/application.js
@@ -99,8 +99,8 @@ var LiveCommentsView = Backbone.View.extend({
     if (body.val() != '') {
       var newComment= new models.Comment({ Author: author.val()
                                          , Body: body.val()
-                                         , PageUrl: '/'});
-      this.socket.send(newComment.toJSON());
+                                         , PageUrl: window.location.origin + window.location.pathname});
+      this.socket.send(JSON.stringify({event: "comment", data: JSON.stringify(newComment.toJSON())}));
       body.val('');
     }
   }
@@ -117,6 +117,9 @@ var LiveCommentsController = {
 
     this.socket.on('message', function(msg) {view.msgReceived(msg)});
     this.socket.connect();
+    this.socket.send(JSON.stringify({ event: "initial"
+                     , data: window.location.origin + window.location.pathname
+                     }));
 
 
     return this;
